5b055dbce67c7780d05e0ffb4fd13c53c7f00dde
Ganesh Sharma <gsk1664@gmail.com>
2021-02-05 11:08:53 +0530

    push root file with lifecycle

--- money/src/App.js
+++ money/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Form from "./component/Form"
+import LifeCycleA from './component/LifeCycleA';
+import LifeCycleB from './component/LifeCycleB';
 
 function App() {
   return (
     <div className="App">
-       <Form />     
+      <LifeCycleA />
     </div>
   );
 }
