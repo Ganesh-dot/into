402114f1899fbf660836fc387d588512d074146e
Ganesh Sharma <gsk1664@gmail.com>
2021-02-15 11:46:27 +0530

    push rootfile

--- money/src/App.js
+++ money/src/App.js
@@ -1,13 +1,15 @@
-import { PureComponent } from 'react';
-import './App.css';
-import FRParentInput from './component/FRParentInput';
-import ParentComp from './component/ParentComp';
-import RefsDemo from "./component/RefsDemo"
+import { PureComponent } from "react";
+import "./App.css";
+import FRParentInput from "./component/FRParentInput";
+import ParentComp from "./component/ParentComp";
+import PortalDemo from "./component/PortalDemo";
+import RefsDemo from "./component/RefsDemo";
 
 function App() {
   return (
     <div className="App">
-       <FRParentInput /> 
+      <h1>App Portal demo</h1>
+      <PortalDemo />
     </div>
   );
 }
