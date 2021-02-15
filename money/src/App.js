b414c29b1b3472f4b7890fc46cac0dfe5752cc27
Ganesh Sharma <gsk1664@gmail.com>
2021-02-15 10:44:57 +0530

    push Refs and Forwarding refs component

--- money/src/App.js
+++ money/src/App.js
@@ -1,11 +1,13 @@
 import { PureComponent } from 'react';
 import './App.css';
+import FRParentInput from './component/FRParentInput';
 import ParentComp from './component/ParentComp';
+import RefsDemo from "./component/RefsDemo"
 
 function App() {
   return (
     <div className="App">
-      <ParentComp />
+       <FRParentInput /> 
     </div>
   );
 }
